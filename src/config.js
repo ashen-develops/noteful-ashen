7db37e4225b9d82267fdf8a84e84963ce7148f6c
ashen-develops <ashenp.designs@gmail.com>
2020-12-10 11:55:56 -0600

    changed a slash to a blank

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://noteful-server-ap.herokuapp.com/'
+  API_ENDPOINT: 'https://noteful-server-ap.herokuapp.com'
   // API_ENDPOINT: 'https://localhost://8000'
 }
